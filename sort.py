50484de1b6a87e57cbc67bad09d01ac8585294de
slimeless <121390535+slimeless@users.noreply.github.com>
2022-12-25 17:25:43 +0300

    Update sort.py

--- sort.py
+++ sort.py
@@ -1,10 +1,36 @@
-import pandas as pd
-df = pd.read_csv("merge_from_ofoct.csv")
-a = df["Продукт"].tolist()
-for i in range(len(a)):
-    a[i] = a[i].lower()
+def sortSearch(a):
+    = =
+    a = str(a).lower().split()
+    a = list(map(lambda x: re.sub(r"[\W]", "", x), a))
+    for i in range(len(a)):
+        if len(a[i]) != 2:
+            inputData.(a[i])
 
-df["Продукт"] = a
+    return inputData
+
+
+def (b):
+    asd = foodfood
+    for i in b:
+        asd = asd[[i in c for c in list(asd['Продукт'])]]
+    return asd
+
+
+def endSearch(c):
+    = = []
+    for i in list (c.index):
+        end.append(c.loc[i,].values.tolist())
+    return end
+
+
+def retypeSearch(d):
+    for i in range (len(d)):
+        for j in range(len(d[0])):
+            0 != != 0:
+                d[i][j] = re.sub(r"[,]", ".", d[i][j])
+                d[i][j] = float(re.sub(r"[^0-9.]", "", d[i][j]))
+
+    return d
 
 
 
